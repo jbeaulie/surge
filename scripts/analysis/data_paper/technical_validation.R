44e203c2c5e27da0e1af9a74fb805b66b60adb2f
Corra <Corra.Joseph@epa.gov>
2025-06-03 15:58:02 -0400

    Adding 4 files used for technical validation data: technical_validation.R, technical_validation_data RDS (which is generated by technical_validation.R), technical_validation.Rmd (which loads the RDS), and a simplified excel spreadsheet of detection limits

---
+++ scripts/analysis/data_paper/technical_validation.R
@@ -0,0 +1,255 @@
+userPath <- paste0(Sys.getenv("USERPROFILE"), 
+                   "/Environmental Protection Agency (EPA)/",
+                   "SuRGE Survey of Reservoir Greenhouse gas Emissions - Documents/")
+
+# R proj folder at SP.
+localName <- if (grepl("JBEAULIE", userPath, ignore.case = TRUE)) {
+  "Jake/"
+} else if (grepl("JCOR", userPath, ignore.case = TRUE)) {
+  "Joe/"} 
+
+# load project libraries
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/masterLibrary.R"))
+
+# load data
+# Analysis
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readSurgeLakes.R")) # read in survey design file
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/chemSampleList.R")) # creates chem.samples.foo, an inventory of all collected chem sample
+
+
+# Read chemistry
+# uncomment rows to bring in more data
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readFieldSheets.R")) # read surgeData...xlsx.  fld_sheet, dg_sheet
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readAnionsAda.R")) # read ADA lab anions
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readAnionsDaniels.R")) # read Kit Daniels anions
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readNutrientsAda.R")) # read nutrients ran in ADA lab
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readNutrientsAwberc.R")) # read AWBERC lab nutrient results
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readNutrientsR10_2018.R")) # read AWBERC nutrients for 2018 R10
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readOcAda.R")) # read ADA TOC/DOC data
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readOcMasi.R")) # read 2020 TOC run at MASI lab
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readTteb.R")) # TTEB metals, TOC, DOC
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readPigmentsMicrocystin.R")) # NAR chl, phyco, and microcystin
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readTaxonomy.R")) # GB taxonomy
+source(paste0(userPath, "rProjects/", localName, "SuRGE/scripts/analysis/readChlorophyllR10_2018.R")) # 2018 R10 chlorophyll
+
+source(paste0(userPath, "rProjects/", localName,
+              "SuRGE/scripts/analysis/mergeChemistry.R")) # merge all chem objects #chemistry all
+
+
+# Data Setup---------------------------------------------
+
+detlimits <- read_xls(path = paste0("detectionLimits.xls"))
+
+# d.anions needs to have flags consolidated into '_flags' columns
+d.anions_formatted <- d.anions %>% 
+  select(!ends_with("units")) %>%
+  unite(starts_with("f_"), col = "f_flags", sep = " ") %>% 
+  unite(starts_with("br_"), col = "br_flags", sep = " ") %>%
+  unite(starts_with("cl_"), col = "cl_flags", sep = " ") %>%
+  unite(starts_with("so4_"), col = "so4_flags", sep = " ")
+
+# Create list of data
+chemdata <- lst(ada.anions, d.anions_formatted, chem18, ada.nutrients, 
+                chemCinNutrients, tteb.all, toc.masi, ada.oc, chl18, pigments) 
+  
+# Do not use phycocyanin
+chemdata$pigments <- chemdata$pigments %>% select(!starts_with("phyco"))
+
+# Create 'visit' column if it doesn't exist 
+chemdata_formatted <- chemdata %>% map(\(.x) 
+                 {if (!"visit" %in% names(.x)) mutate(.x, visit = 1) 
+                   else .x}) 
+
+# Flags-------------------------------------------------
+
+tally_flags <- function(data) { 
+  
+  data <- data %>%
+  ungroup() %>%
+    select(!c(site_id, sample_depth), -matches("units")) %>%
+    filter(sample_type != "blank") %>%
+    pivot_longer(ends_with("flags"), names_to = "flag_names", values_to = "flags") %>%
+    mutate(flag_names = if_else(flag_names == "no2_3_flags", "no2.3_flags", flag_names), 
+           # Remove 'tteb.' from analyte names
+           flag_names = str_remove(flag_names, "tteb."), 
+      analyte = word(flag_names, sep = "_")) %>%
+    group_by(sample_type, analyte) %>%
+    summarize(ND = sum(str_detect(flags, "ND"), na.rm = TRUE), 
+              L = sum(str_detect(flags, "L"), na.rm = TRUE), 
+              H = sum(str_detect(flags, "H"), na.rm = TRUE), 
+              S = sum(str_detect(flags, "S"), na.rm = TRUE), 
+              total = n()) %>%
+    ungroup()
+  
+  return(data)
+  
+}
+
+# Create summary tables of flags by analyte and sample type
+flags <- chemdata_formatted %>% map(\(.x) tally_flags(.x))
+
+
+# Field Blanks------------------------------------------
+
+ summarize_blanks <- function(df, groupname) { 
+   
+   df <- df %>%
+     ungroup() %>%
+     mutate(sample_type = case_when(sample_type == "duplicate" ~ "unknown", TRUE ~ sample_type)) %>%
+     select(!c(site_id, sample_depth), -matches("flag|units")) %>%
+     pivot_longer(!c(lake_id, sample_type, visit)) %>%
+     group_by(lake_id, name, sample_type, visit) %>%
+     summarize(min = min(value, na.rm = TRUE), 
+               max = max(value, na.rm = TRUE)) %>%
+     ungroup() %>%
+     pivot_wider(names_from = sample_type, values_from = c(min, max)) %>%
+     filter(is.na(min_blank) == FALSE, is.infinite(min_blank) == FALSE) %>%
+     select(-max_blank) %>%
+     mutate(name = str_remove(name, "tteb."))
+   
+   df <- df %>%
+     # apply conditional filtering to detlimits based on dataframe name
+     left_join(y = {if (groupname %in% c("ada.anions", "ada.nutrients", "ada.oc")) 
+       filter(detlimits, lab == "ADA")
+       else if (groupname %in% c("d.anions_revised", "tteb.all", "toc.masi")) 
+         filter(detlimits, lab == "CIN")
+       else if (groupname == "chem18") 
+         filter(detlimits, lab == "CIN" & year == "2018")
+       else filter(detlimits, lab == "CIN" & year == "2021") }, by = "name") %>%
+     select(-lab, -year, -units) %>%
+     rename(analyte = name, blank = min_blank, 
+            minimum = min_unknown, maximum = max_unknown) %>%
+     mutate(mean_unknown = (minimum + maximum) / 2, 
+            blank_prop = round((blank / mean_unknown), 2))
+   
+    # mutate(blank_v_unknown = case_when(blank < minimum ~ blank/minimum, 
+    #                                    blank > maximum ~ blank/maximum,
+    #                                    blank >= minimum & blank <= maximum ~ 0, 
+    #                                    .default = NA
+    #        ))
+
+ }
+ 
+# 'pigments' has no blanks
+chemdata_formatted$pigments <- NULL 
+
+  blanks <- imap(chemdata_formatted, summarize_blanks) %>%
+    list_rbind() %>%
+    group_by(analyte_group) %>%
+    group_split()
+ 
+  # Don't understand this... found in toc.masi and ada.oc
+  # mutate(mdl = sprintf("%.2f", mdl))
+  # mutate(mdl = sprintf("%.2f", mdl)) 
+
+# Field Duplicates------------------------------------------
+
+## Field Duplicates Setup----------------------------------
+  
+# identify lakes, sites, and depths with duplicates
+duplicate_ids <- chemistry_all %>% filter(sample_type == "duplicate") %>%
+  select(lake_id, site_id, sample_depth, visit) %>%
+  mutate(id = paste0(lake_id, site_id, sample_depth, visit))
+
+# pull out data from lakes, sites, and depths with duplicates
+chemistry_all_dups <- chemistry_all %>%
+  mutate(id = paste0(lake_id, site_id, sample_depth, visit)) %>% # create unique id
+  filter(id %in% duplicate_ids$id) %>% # pull out duplicates and corresponding unknowns
+  select(-contains("flag"), -contains("qual"), -contains("units"),
+         -sample_type, -id, -no3) %>%
+  pivot_longer(!c(lake_id, site_id, sample_depth, visit))
+
+## Nutrients----------------------------------
+nutrient_name <- detlimits %>% 
+  filter(analyte_group == "nutrients") %>%
+  select(name) %>% 
+  pull()
+
+# calculate mean relative percent difference among field replicates
+nutrients_rpd <- chemistry_all_dups %>% 
+  filter(name %in% nutrient_name) %>%
+  group_by(lake_id, site_id, sample_depth, name, visit) %>%
+  mutate(ad = abs(diff(value)), # difference between dup and unknown
+         mean = mean(value, na.rm = TRUE), # mean of dup and unknown
+         rpd = (ad/mean)*100) %>% # rpd
+  distinct() 
+
+
+## Anions----------------------------------
+anion_name <- detlimits %>% 
+  filter(analyte_group == "anions") %>%
+  select(name) %>% 
+  pull()
+
+# calculate mean relative percent difference among field replicates
+anions_rpd <- chemistry_all_dups %>% 
+  filter(name %in% anion_name) %>%
+  group_by(lake_id, site_id, sample_depth, name, visit) %>%
+  mutate(ad = abs(diff(value)), # difference between dup and unknown
+         mean = mean(value), # mean of dup and unknown
+         rpd = (ad/mean)*100) %>% # rpd
+  distinct() 
+
+## Metals----------------------------------
+
+metal_name <- detlimits %>% 
+  filter(analyte_group == "metals") %>%
+  select(name) %>% 
+  pull()
+
+# calculate mean relative percent difference among field replicates
+metals_rpd <- chemistry_all_dups %>% 
+  filter(name %in% metal_name) %>%
+  group_by(lake_id, site_id, sample_depth, name, visit) %>%
+  mutate(ad = abs(diff(value)), # difference between dup and unknown
+         mean = mean(value), # mean of dup and unknown
+         rpd = (ad/mean)*100) %>% # rpd
+  distinct() 
+
+## Organic Carbon----------------------------------
+
+organic_name <- detlimits %>% 
+  filter(analyte_group == "organic") %>%
+  select(name) %>% 
+  pull()
+
+# calculate mean relative percent difference among field replicates
+organic_rpd <- chemistry_all_dups %>% 
+  filter(name %in% organic_name) %>%
+  group_by(lake_id, site_id, sample_depth, name, visit) %>%
+  mutate(ad = abs(diff(value)), # difference between dup and unknown
+         mean = mean(value), # mean of dup and unknown
+         rpd = (ad/mean)*100) %>% # rpd
+  distinct() 
+
+
+## Pigments----------------------------------
+
+chlorophyll_rpd <- chemistry_all_dups %>% 
+  filter(name == "chla_lab") %>%
+  group_by(lake_id, site_id, sample_depth, name, visit) %>%
+  mutate(ad = abs(diff(value)), # difference between dup and unknown
+         mean = mean(value), # mean of dup and unknown
+         rpd = (ad/mean)*100) %>% # rpd
+  distinct()
+
+
+dupes <- lst(anions_rpd, nutrients_rpd, chlorophyll_rpd, metals_rpd, organic_rpd)
+
+
+# Tables-----------------------------------
+
+# Flags table
+
+flags_table <- tribble(
+  ~Condition, ~ Value, ~Flag,
+ "< MDL", "MDL",	"ND",
+   "MDL and < RL",	"Measured concentration",	"L",
+  "Holding time violation",	"Measured concentration",	"H",
+  "Shipping issue",	"Measured concentration",	"S"
+) 
+
+technical_validation_data <- lst(dupes, blanks, flags, detlimits, chemistry_all)
+
+saveRDS(technical_validation_data, "scripts/analysis/data_paper/technical_validation_data")
+
