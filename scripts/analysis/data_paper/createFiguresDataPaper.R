0a817807f0c4d4bfebff9e40983f6b0d85b1bc71
Deemer <bdeemer@usgs.gov>
2025-06-17 21:33:43 -0700

    Save figure files with 300 dpi using ggsave

--- scripts/analysis/data_paper/createFiguresDataPaper.R
+++ scripts/analysis/data_paper/createFiguresDataPaper.R
@@ -11,27 +11,27 @@ tot<-data.frame(dat$ch4_total,"total")
 colnames(tot)<-c("rate","type")
 dieb<-bind_rows(di,eb,tot)
 
-options(scipen = 999)
-densplot<-dieb%>%
-  mutate(rt=rate*24)%>%
-  mutate(rtc=rt*(12.01/16.043))%>%
-  ggplot(aes(x=rt,color=type,fill=type))+
-  geom_density(alpha=0.1)+
-  scale_color_manual(values = c("#56B4E9","#009E73","#D55E00"))+
-  scale_fill_manual(values = c("#56B4E9","#009E73","#D55E00"))+
-  theme_bw()+
-  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
-        axis.line=element_line(colour="black"),legend.title=element_blank(),
-        axis.text = element_text(size = 14),
-        axis.title = element_text(size = 16),
-        # axis.text.x = element_blank(),
-        # axis.labels.x = element_blank(),
-        # axis.ticks.x = element_blank(),
-        legend.position="top")+
-  scale_x_log10(limits=c(0.001,10000))+
-  xlab(expression(paste("Methane (mg CH"[4]*" m"^"-2"*"d"^"-1"*")")))+
-  ylab("Density")
-densplot
+# options(scipen = 999)
+# densplot<-dieb%>%
+#   mutate(rt=rate*24)%>%
+#   mutate(rtc=rt*(12.01/16.043))%>%
+#   ggplot(aes(x=rt,color=type,fill=type))+
+#   geom_density(alpha=0.1)+
+#   scale_color_manual(values = c("#56B4E9","#009E73","#D55E00"))+
+#   scale_fill_manual(values = c("#56B4E9","#009E73","#D55E00"))+
+#   theme_bw()+
+#   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
+#         axis.line=element_line(colour="black"),legend.title=element_blank(),
+#         axis.text = element_text(size = 14),
+#         axis.title = element_text(size = 16),
+#         # axis.text.x = element_blank(),
+#         # axis.labels.x = element_blank(),
+#         # axis.ticks.x = element_blank(),
+#         legend.position="top")+
+#   scale_x_log10(limits=c(0.001,10000))+
+#   xlab(expression(paste("Methane (mg CH"[4]*" m"^"-2"*"d"^"-1"*")")))+
+#   ylab("Density")
+# densplot
 
 #create an object for overlaid carbon dioxide density plots
 dic<-data.frame(dat$co2_diffusion_best,"diffusion")
@@ -45,51 +45,10 @@ diebc<-bind_rows(dic,ebc,totc)
 nbreaks <- 7
 breaks <-c(-10^(nbreaks:1),0, 10^(nbreaks:1))
 
-# densplotco2<-diebc%>%
-#   mutate(rt=rate*24)%>%
-#   mutate(rtc=rt*(12.01/44.009))%>%
-#   ggplot(aes(x=rt,color=type,fill=type))+
-#   geom_density(alpha=0.1)+
-#   scale_color_manual(values = c("#56B4E9","#009E73","#D55E00"))+
-#   scale_fill_manual(values = c("#56B4E9","#009E73","#D55E00"))+
-#   theme_bw()+
-#   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
-#         axis.line=element_line(colour="black"),legend.title=element_blank(),
-#         axis.text = element_text(size = 14,angle = 90),
-#         axis.title = element_text(size = 16),
-#         legend.position="none")+
-#   scale_x_continuous(trans = "pseudo_log",breaks=breaks)+
-#   xlab(expression(paste("Carbon Dioxide (mg CO"[2]*" m"^"-2"*"d"^"-1"*")")))+
-#   ylab("Density")
-# densplotco2
-# 
-# densplotco2b<-totc%>%
-#   mutate(rt=rate*24)%>%
-#   mutate(rtc=rt*(12.01/44.009))%>%
-#   ggplot(aes(x=rt,color=type,fill=type))+
-#   geom_density(alpha=0.1)+
-#   scale_color_manual(values = "#D55E00")+
-#   scale_fill_manual(values = "#D55E00")+
-#   geom_vline(xintercept=0)+
-#   theme_bw()+
-#   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
-#         axis.line=element_line(colour="black"),legend.title=element_blank(),
-#         axis.text = element_text(size = 14,angle = 90),
-#         axis.title = element_text(size = 16),
-#         legend.position="none")+
-#   xlab(expression(paste("Carbon Dioxide (mg CO"[2]*" m"^"-2"*"d"^"-1"*")")))+
-#   ylab("Density")
-# densplotco2b
-# 
-# dens<-cowplot::plot_grid(densplot,densplotco2,ncol=1,align="v",labels=c("A","B"),rel_heights = c(1,1))
-# dens
-
-
-
 
 ## rmp additions
 
-dat_all = read.csv("communications/manuscript/data_paper/6_emission_rate_points.csv") %>%
+dat_all = read.csv("communications/manuscript/data_paper/4_emission_rate_points.csv") %>%
   select(ch4_diffusion, ch4_ebullition, ch4_total, co2_diffusion, co2_ebullition, co2_total) %>%
   pivot_longer(values_to = "rate_hourly", names_to = "name", cols = ch4_diffusion:co2_total) %>%
   separate(name, into = c("gas_name", "type"), sep = "_") %>%
@@ -98,7 +57,7 @@ dat_all = read.csv("communications/manuscript/data_paper/6_emission_rate_points.
          rate_daily = rate_hourly * 24)
 
 
-ggplot() +
+fig_7<-ggplot() +
   geom_density(data = dat_all, aes(x = rate_daily, y=..scaled.., fill = type, color = type), alpha = 0.3,
                trim = T) +
   geom_vline(xintercept = 0, linetype = 2) +
@@ -119,6 +78,9 @@ ggplot() +
         legend.background = element_rect(color = "black"))+
   xlab(expression(paste("Emissions Rate (mg m"^"-2"~"d"^"-1"*")")))+
   ylab("Density (scaled)")
+
+ggsave("scripts/analysis/data_paper/figure_7.jpeg",width=6.5,height=4,
+      dpi=300)
 
 ### Unstable start plot
 
@@ -144,12 +106,14 @@ CO2<-unstable_plot_data %>%
   theme_bw()+
   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
         axis.line=element_line(colour="black"),legend.title=element_blank(),
-        axis.text = element_text(size = 14),
-        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12),
         axis.text.x = element_blank(),
         axis.labels.x = element_blank(),
         axis.ticks.x = element_blank(),
-        legend.position="top")+
+        legend.position="top",
+        legend.text = element_text(size =10))+
+        guides(color=guide_legend(nrow=2,byrow=TRUE))+
   geom_vline(aes(xintercept = as.numeric(as.POSIXct("2021-06-28 17:16:22", tz = "UTC")),
                  color = "deployment"), key_glyph = "path") +
   geom_vline(aes(xintercept = as.numeric(as.POSIXct("2021-06-28 17:17:08", tz = "UTC")),
@@ -171,8 +135,8 @@ CH4<-unstable_plot_data %>%
   theme_bw()+
   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
         axis.line=element_line(colour="black"),legend.title=element_blank(),
-        axis.text = element_text(size = 14),
-        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12),
         axis.text.x = element_blank(),
         axis.labels.x = element_blank(),
         axis.ticks.x = element_blank(),
@@ -199,8 +163,8 @@ H2O<-unstable_plot_data %>%
   theme_bw()+
   theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
         axis.line=element_line(colour="black"),legend.title=element_blank(),
-        axis.text = element_text(size = 14),
-        axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12),
         legend.position="none")+
   geom_vline(aes(xintercept = as.numeric(as.POSIXct("2021-06-28 17:16:22", tz = "UTC")),
                  color = "deployment"), key_glyph = "path") +
@@ -216,10 +180,10 @@ H2O<-unstable_plot_data %>%
   ylab(expression(paste("H "[2]*"O (ppt)")))
 H2O
 
-unstab<-cowplot::plot_grid(CO2,CH4,H2O,ncol=1,align="v",labels=c("A","B","C"),rel_heights = c(1.1,1,1))
+unstab<-cowplot::plot_grid(CO2,CH4,H2O,ncol=1,align="v",labels=c("A","B","C"),rel_heights = c(1.4,1,1))
 unstab
 
-
+ggsave("scripts/analysis/data_paper/figure_3.jpeg",width=3.25,height=6,dpi=300)
 
 
 ## UPDATED:  Plot of Variables that Survey was Designed on
